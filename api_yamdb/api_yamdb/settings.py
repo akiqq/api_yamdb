bfaf00dddac5b598b0598e5d85083e3f2e6346a1
Alexandr Ainullin <aainullin@gmail.com>
2023-01-27 00:12:29 +0300

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
+    'rest_framework_simplejwt',
+    'djoser',
     'api',
     'reviews',
     'users'
